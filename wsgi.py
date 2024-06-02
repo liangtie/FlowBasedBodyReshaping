42d0caad666624ad9e7f3b998dbfb903a9353d4f
Ethan Chien <liangtie.qian@gmail.com>
2024-06-02 16:51:38 +0800

    Add wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,15 @@
 # from flask_json import FlaskJSON, JsonError, json_response, as_json
+import json
 from flask import Flask, request
+from flask import Flask, request, jsonify, send_file
+
 from flask_cors import CORS
 from playhouse.shortcuts import model_to_dict
 
 # sudo lsof -i :7565
 import base64
 import os
+from reshaping import ReShaping
 import utils
 
 
@@ -13,41 +17,49 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/is_valid_license', methods=['POST'])
-def is_valid_license():
+@app.route('/body_shaping', methods=['POST'])
+def body_shaping():
 
-    is_valid =  False
-    error = None
+   # Check if the post request has the file part
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part in the request"}), 400
+    
+    file = request.files['file']
+    
+    # If the user does not select a file, the browser submits an empty file without a filename
+    if file.filename == '':
+        return jsonify({"error": "No selected file"}), 400
+
+  # Get JSON parameters
+    json_params = request.form.get('json')
+    if not json_params:
+        return jsonify({"error": "No JSON parameters provided"}), 400
+
+    json_data = None
 
     try:
-        license =  request.form['license']
-        mac =  request.form['mac']
+        json_data = json.loads(json_params)
+    except Exception as e:
+        return jsonify({"error": "Invalid JSON format"}), 400
 
-        def do_valid_license(license , usr_mac):
-            d =utils.base64_decode(license)
+    reshape_worker = ReShaping()
 
-            b = d.split(SPLITTER)
-            if len(b) != 2:
-                return False , "Invalid license"
-            mac , expiration_time = b
+    try :
+        reshape_worker.reshape_body(file, json_data['degree'], json_data['roi'])
+    except Exception as e:
+        return jsonify({"error": f'{e}'}), 400
 
-            if utils.current_datetime_in_seconds() > int(expiration_time):
-                return False , "License expired"
+    res_img = reshape_worker.get_out_img_path()
 
-            if mac != usr_mac:
-                return False , "License not match"
+    if res_img == None:
+        return jsonify({"error": "supported image format is '.png', '.jpg', '.jpeg','.JPG'"}), 400
 
-            return True , None
-
-        is_valid , error= do_valid_license(license ,mac)
-
-    except (KeyError, TypeError, ValueError) as e:
-        error = f'{str(e) } ; {error}'
-
-    return {"is_valid" : is_valid , "error" : error}
+    response = send_file(res_img, mimetype=file.mimetype)
+    reshape_worker.clear_up_dirs()
+    return response
 
 if __name__ == '__main__':
-    from gevent import pywsgi
     print("starting srv")
+    from gevent import pywsgi
     server = pywsgi.WSGIServer(('0.0.0.0', 7565), app)
     server.serve_forever()
